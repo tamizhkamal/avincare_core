8d37e1db0a7e596916335698dddcec1fc8a31775
tamizh_kamal <tamizhkamal6590@gmail.com>
2026-03-08 13:25:09 +0530

    cd d:\STIT\pharma\avincare_core; python manage.py collectstatic --noinput added

--- avincare_core/settings.py
+++ avincare_core/settings.py
@@ -120,7 +120,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'asserts')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
+# Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Production static files settings
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
